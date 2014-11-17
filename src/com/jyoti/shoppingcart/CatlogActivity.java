76e1fda0be526e043f2d7711151cab6f463ac51f
jyoti <jsakhare@IMG-109-v1.local>
2014-11-17 13:12:21 +0530

    shoppingcart

---
+++ src/com/jyoti/shoppingcart/CatlogActivity.java
@@ -0,0 +1,55 @@
+package com.jyoti.shoppingcart;
+
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class CatlogActivity extends Activity {
+
+ private List<Product> mProductList;
+
+ /** Called when the activity is first created. */
+ @Override
+ public void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.catlog);
+
+  // Obtain a reference to the product catalog
+  mProductList = ShoppingcartHelper.getCatalog(getResources());
+  
+  // Create the list
+  ListView listViewCatalog = (ListView) findViewById(R.id.ListViewCatalog);
+  listViewCatalog.setAdapter(new ProductAdaptor(mProductList, getLayoutInflater(), false));
+  
+  listViewCatalog.setOnItemClickListener(new OnItemClickListener() {
+
+   @Override
+   public void onItemClick(AdapterView<?> parent, View view, int position,
+     long id) {
+    Intent productDetailsIntent = new Intent(getBaseContext(),ProductDetailsActivity.class);
+    productDetailsIntent.putExtra(ShoppingcartHelper.PRODUCT_INDEX, position);
+    startActivity(productDetailsIntent);
+   }
+  });
+  
+  Button viewShoppingCart = (Button) findViewById(R.id.ButtonViewCart);
+  viewShoppingCart.setOnClickListener(new OnClickListener() {
+   
+   @Override
+   public void onClick(View v) {
+    Intent viewShoppingCartIntent = new Intent(getBaseContext(), ShoppingCartActivity.class);
+    startActivity(viewShoppingCartIntent);
+   }
+  });
+
+ }
+}
